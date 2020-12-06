3f659cae5cf026db8ef7dac39151187c0da18667
DarkMystical <37736875+DarkMystical@users.noreply.github.com>
2020-12-05 22:32:24 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include "view/header.php"; ?>
+
+<main>
+    <nav>
+        </br>
+        </br>
+     <h2> Welcome to Course Portal at Auburn University at Montgomery </h2> </br></br>
+    <ul>
+        </br>
+        <li><a href="register">Register/</a></li>
+        </br>
+        <li><a href="login">Login/</a></li>
+        </br>
+        </br>
+    </ul>
+    </nav>
+    </br>
+</main>
+<?php include("view/footer.php"); ?>
